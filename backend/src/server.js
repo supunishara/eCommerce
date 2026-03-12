c867ee22b178eb4d780725ee9262e3b18d5e7514
supunishara <supunishara3@gmail.com>
2026-03-13 01:09:10 +0530

    configuration changes and startServer method added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import path from "path";
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 
 const __dirname = path.resolve();
+
+app.use(clerkMiddleware());
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
@@ -18,4 +22,16 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-app.listen(ENV.PORT, () => console.log("Server is running 123"));
+// app.listen(ENV.PORT, () => {
+//   console.log("Server is running 123");
+//   connectDB();
+// });
+
+const startServer = async () => {
+  await connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log("Server is running 123");
+  });
+};
+
+startServer();
